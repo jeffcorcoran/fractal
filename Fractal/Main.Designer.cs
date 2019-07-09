6587f26b4fb7a085555e91bdded06acb9ec6b4c6
Jeffrey Corcoran <jcorcoran@fetchlogistics.com>
2019-07-09 08:54:47 -0400

    Add project files.

---
+++ Fractal/Main.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Fractal
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMain
+            // 
+            this.pbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbMain.Location = new System.Drawing.Point(0, 0);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(800, 450);
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            this.pbMain.MouseEnter += new System.EventHandler(this.PbMain_MouseEnter);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbMain);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMain;
+    }
+}
+
---
+++ Fractal/Main.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Fractal
+{
+    public partial class Main : Form
+    {
+        double _xZoom = 0.0d;
+        double _yZoom = 0.0d;
+
+        double _baseXZoom = 4;
+        double _baseYZoom = 4;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            _xZoom = (_baseXZoom / Convert.ToDouble(pbMain.Width));
+            _yZoom = (_baseYZoom / Convert.ToDouble(pbMain.Height));
+
+            Bitmap pic = Mandelbrot(2, 2);
+            pbMain.Image = pic;
+
+            //pbMain.MouseWheel += PbMain_MouseWheel;
+        }
+
+        private void PbMain_MouseWheel(object sender, MouseEventArgs e)
+        {
+            _xZoom += 1d;
+            _yZoom += 1d;
+            Bitmap pic = Mandelbrot(2, 2);
+            pbMain.Image = pic;
+        }
+
+        private Bitmap Mandelbrot(double Xmin, double Ymin)
+        {
+            Bitmap pic = new Bitmap(pbMain.Width, pbMain.Height);
+
+            double zx = 0;
+            double zy = 0;
+            double cx = 0;
+            double cy = 0;
+            double tempzx = 0;
+
+            int loopgo = 0;
+
+            for (int x = 0; x < pic.Width; x++)
+            {
+                cx = (_xZoom * x) - Math.Abs(Xmin);
+                for (int y = 0; y < pic.Height; y++)
+                {
+                    zx = 0;
+                    zy = 0;
+                    cy = (_yZoom * y) - Math.Abs(Ymin);
+                    loopgo = 0;
+
+                    while (zx * zx + zy * zy <= 4 && loopgo < 1000)
+                    {
+                        loopgo++;
+                        tempzx = zx;
+                        zx = (zx * zx) - (zy * zy) + cx;
+                        zy = (2 * tempzx * zy) + cy;
+                    }
+
+                    if (loopgo != 1000)
+                    {
+                        pic.SetPixel(x, y, Color.FromArgb(loopgo % 128 * 2, loopgo % 32 * 7, loopgo % 16 * 14));
+                    }
+                    else
+                    {
+                        pic.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+
+            return pic;
+        }
+
+        private void PbMain_MouseEnter(object sender, EventArgs e)
+        {
+            pbMain.Focus();
+        }
+    }
+}
